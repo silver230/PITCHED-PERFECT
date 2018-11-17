62ed718c1e5470a9f9f610e8ff701ab87b3c7c99
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-17 14:49:19 +0300

    renamed the file

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,16 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-
 def create_app(config_name):
     app = Flask(__name__)
-
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://moringaschool:1204@localhost/test'
     app.config.from_object(config_options[config_name])
-
+    
+    
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
