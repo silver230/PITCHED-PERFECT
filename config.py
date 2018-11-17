90d03131db8a42712b6ddc232afbac95b66b4acf
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-17 11:20:06 +0300

    created my config class

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://yyvonne:ojijo@localhost/my_pitch'
+      
+class ProdConfig(Config):
+ 
+ 
+class DevConfig(Config):
+    DEBUG = True
+ 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+} 
+
+     
