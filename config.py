8f17f7969eab7832a441db53205115da6af6f231
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-17 14:47:53 +0300

    blocked class ProdConfig for my DevConfig class to pass

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1204@localhost/my_pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1204@localhost/pitched_perfect'
       
 class ProdConfig(Config):
- 
- 
+    pass
+
 class DevConfig(Config):
+
     DEBUG = True
  
 config_options = {
