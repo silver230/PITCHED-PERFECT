15be1e5687285822037d56547d4460c983a0fc46
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-19 10:22:16 +0300

    added specific place where photos will be placed ,mailserver setted my debug mode to be true an tested

--- config.py
+++ config.py
@@ -1,20 +1,35 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = '120dsaf4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1204@localhost/pitched_perfect'
-      
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'sylveromondipitched_perfect'
+    SENDER_EMAIL = 'sylveromondi@gmail.com'
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
 class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-
     DEBUG = True
- 
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringashool:1204@localhost/my_pitch_test'
+class DevConfig(Config):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1204@localhost/pitched_perfect'
+
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
-} 
-
-     
+'production':ProdConfig,
+# 'test':TestConfig
+}    
