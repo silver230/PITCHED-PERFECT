64265068c498f5f9b98138654c1c6fabf64791f6
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-19 10:26:51 +0300

    imported  my db,Models,manager loader,shell and command to run the progrm

--- manage.py
+++ manage.py
@@ -1,18 +1,17 @@
-from app.models import User,Role
+from app import create_app ,db
 from flask_script import Manager,Server
-from app import create_app,db
-from  flask_migrate import Migrate, MigrateCommand
-
+from app.models import User,Pitch,Comment
+from flask_migrate import Migrate , MigrateCommand
 
 app = create_app('development')
+# app = create_app('production')
 manager = Manager(app)
-manager.add_command('server',Server)
-
 migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role  )
+    return dict(app = app,db = db,User = User)
+
 if __name__ == '__main__':
    manager.run()
